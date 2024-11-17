68fb1a2fac8f192fc9c312fae779c1de2054bd47
Itaybo89 <itaybo89@gmail.com>
2024-11-17 20:12:34 +0200

    stsarting add truck

--- Billing-Team/functions/add_truck.py
+++ Billing-Team/functions/add_truck.py
@@ -1,31 +1,33 @@
 from sqlalchemy.orm import sessionmaker
-from sql.billing_sql import Providers, engine
+from sql.billing_sql import Truck, engine
 
 # Create a session maker bound to the engine
 Session = sessionmaker(bind=engine)
 session = Session()
 
-def create_provider(data):
-    name = data.get('name')
-    if not name:
-        return {"error": "Name is required"}, 400
+def add_truck(data):
+    provider_id = data.get('provider_id')
+    id = data.get('id')
+    
+    if not id and provider_id:
+        return {"error": "id and provider_id is required"}, 400
 
     try:
         # Create a new instance of the Providers class
-        new_provider = Providers(name=name)
+        new_truck = Truck(id=id, provider_id=provider_id)
 
         # Add the new provider to the session
-        session.add(new_provider)
+        session.add(new_truck)
 
         # Commit the transaction to persist the data
         session.commit()
 
         # Return the created provider details
-        return {"id": new_provider.id, "name": new_provider.name}, 201
+        return {"id": new_truck.id, "provider id": new_truck.provider_id}, 201
     except Exception as e:
         # Rollback in case of an error
         session.rollback()
-        return {"error": f"Failed to create provider: {str(e)}"}, 500
+        return {"error": f"Failed to add truck: {str(e)}"}, 500
 
 
         
--- Billing-Team/routes.py
+++ Billing-Team/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from functions.create_provider import create_provider
+from functions.add_truck import add_truck
 
 # Define a Blueprint for provider-related routes
 provider_bp = Blueprint('provider', __name__)
@@ -13,6 +14,16 @@ def provider():
     # Call create_provider function and handle its response
     provider, status_code = create_provider(data)
     return jsonify(provider), status_code
+
+@provider_bp.route('/truck', methods=['POST'])
+def truck():
+    data = request.get_json()
+    if data is None or not isinstance(data, dict):
+        return jsonify({"error": "Invalid JSON or empty request body"}), 400
+
+    #Call add_truck function and handle its response
+    response, status_code = add_truck(data)
+    return jsonify(response), status_code
 
 # Setup routes by registering the blueprint
 def setup_routes(app):
