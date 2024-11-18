642c319430372fd83ad455e1acb912cafbd8ba62
iddchn <idan@nexil.co.il>
2024-11-18 11:04:47 +0200

    added get_rates functiona and routes

--- billing_team/functions/post_rates.py
+++ billing_team/functions/post_rates.py
@@ -1,11 +1,19 @@
 import pandas as pd
 from sqlalchemy import update, and_, insert
 from sql.billing_sql import Rates, session
+from flask import jsonify
 
 execl_data = []
 
+#get the data from the execl file and proces it to object for storing in the BD
 def getting_execl_data(file):
-    df = pd.read_excel(file)
+    file_path = f"../moc/{file}"
+    try:
+        df = pd.read_excel(file_path)
+    except Exception as e:
+        log = f"error: no file found {str(e)}"
+        return {"error": log} , 400
+
     for i in df.index:
         execl_data.append({
             'product' : df.Product[i],
@@ -17,9 +25,10 @@ def getting_execl_data(file):
     rates_update = update_rate_db()
     return rates_update
 
+#update the reates in the DB
 def update_rate_db():
     for prod in execl_data:
-        print(f"prod = {prod}")
+        # print(f"prod = {prod}")
         product_element = session.query(Rates).filter(
             and_(Rates.product_id == prod['product'],
                  Rates.scope == prod['scope']
@@ -27,19 +36,19 @@ def update_rate_db():
         ).all()
         if (product_element):
             for element in product_element:
-                print(f"true {element.__dict__}")
+                # print(f"true {element.__dict__}")
                 stmt = update(Rates).where(
                     and_(Rates.product_id == prod['product'], Rates.scope == f"{prod['scope']}")
                 ).values(rate = prod['rate'])
                 session.execute(stmt)
                 session.commit()
         else:
-            print(f"{prod['product']} not exist")
+            # print(f"{prod['product']} not exist")
             stmt = insert(Rates).values(product_id = prod['product'], rate = prod['rate'], scope = f"{prod['scope']}")
             session.execute(stmt)
             session.commit()
-            print(f"create {stmt}")
-    return 200
+            # print(f"create {stmt}")
+    return jsonify("All the rates are updated"), 200
 
 if __name__ == '__main__':
     getting_execl_data(file)
--- billing_team/routes.py
+++ billing_team/routes.py
@@ -1,10 +1,54 @@
-from flask import Blueprint, request, jsonify
-from functions import post_rates
+from http.client import responses
 
-app = Flask(__name__)
+from flask import Blueprint, request, jsonify, make_response
 
-@app.route('/rates', methods=['GET', 'POST'])
-def update_rates():
+from functions.create_provider import create_provider
+from functions.get_rates import get_rates_db
+from functions.health_check import check_health
+from functions.post_rates import getting_execl_data
 
 
+# Define a Blueprint for provider-related routes
+provider_bp = Blueprint('provider', __name__)
+
+@provider_bp.route('/provider', methods=['POST'])
+def provider():
+    data = request.get_json()
+    if data is None or not isinstance(data, dict):
+        return jsonify({"error": "Invalid JSON or empty request body"}), 400
+
+    # Call create_provider function and handle its response
+    provider, status_code = create_provider(data)
+    return jsonify(provider), status_code
+
+
+# Define a Blueprint for health-related routes
+health_bp = Blueprint('health', __name__)
+
+@health_bp.route('/health', methods=['GET'])
+def health():
+    # Call the health check function
+    status, status_code = check_health()
+    return jsonify(status), status_code
+
+post_rates_bp = Blueprint('post_rates', __name__)
+
+@post_rates_bp.route('/rates', methods=['POST'])
+def post_rates():
+    response = getting_execl_data(request.args.get('filename'))
+    print(response)
+    return response
+
+get_rates_bp = Blueprint('get_rates', __name__)
+
+@get_rates_bp.route('/rates', methods=['GET'])
+def getting_execl_file():
+    execl_file = get_rates_db()
+    return execl_file
+
+# Setup routes by registering the blueprint
 def setup_routes(app):
+    app.register_blueprint(provider_bp)
+    app.register_blueprint(health_bp)
+    app.register_blueprint(post_rates_bp)
+    app.register_blueprint(get_rates_bp)
